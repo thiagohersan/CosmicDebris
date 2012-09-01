d8e2917d9f64c536038ebe33e88d39196f5bfc7b
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-09-01 01:28:05 -0300

    first version of simple particle class. needs tested

--- src/SimpleParticle.cpp
+++ src/SimpleParticle.cpp
@@ -17,15 +17,52 @@ ofVec2f SimpleParticle::getPos() {
     return pos;
 }
 
+// setters
 void SimpleParticle::setColor(ofColor c) {
     myColor = c;
 }
+void SimpleParticle::setAlpha(float a) {
+	myColor = ofColor(myColor,a);
+}
+void SimpleParticle::setRadius(float r) {
+	myRadius = r;
+}
+void SimpleParticle::setVel(float vv) {
+	velVal = ofClamp(vv, -3, 3);
+}
+
+
 
 bool SimpleParticle::isDead() {
     // if out of bounds... or speed = 0
-    if ((pos.x-myRadius>ofGetWidth()) || (pos.x+myRadius<0) || (pos.y-myRadius>ofGetHeight()) || (pos.y+myRadius<0) || (vel.squareLength() < 0.5)) {
+    if ((pos.x-myRadius>ofGetWidth()) || (pos.x+myRadius<0) || (pos.y-myRadius>ofGetHeight()) || (pos.y+myRadius<0) || (vel.squareLength() < 0.25)) {
 		return true;
     }
     return false;
+}
+
+void SimpleParticle::setTarget(ofVec2f tv) {
+    // unit vector towards target??
+    vel = pos - tv;
+    if (vel.squareLength() > 1) {
+		vel.normalize();
+    }
+    vel *= velVal;
+}
+
+
+void SimpleParticle::update() {
+    vel += acc;
+    pos += vel;
+}
+void SimpleParticle::draw() {
+	ofPushMatrix();
+	ofTranslate(pos);
+	ofSetColor(myColor);
+	ofFill();
+	ofCircle(pos, myRadius);
+	ofNoFill();
+	ofCircle(pos, myRadius);
+	ofPopMatrix();
 }
 
--- src/SimpleParticle.h
+++ src/SimpleParticle.h
@@ -12,7 +12,15 @@ public:
 
 	ofVec2f getPos();
 	void setColor(ofColor c);
+	void setAlpha(float a);
+	void setRadius(float r);
+	void setVel(float vv);
 	bool isDead();
+	void setTarget(ofVec2f tv);
+	
+	void update();
+	void draw();
+
 private:
 	ofVec2f pos, vel, acc;	
 	ofColor myColor;
